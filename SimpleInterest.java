c1321619eb18f04fc2961a53c92d33f7b1fcb1fe
ShreyaRajalbandi <rajalabandishreya2005@gmail.com>
2025-06-10 10:18:43 +0530

    simpleinterest

---
+++ SimpleInterest.java
@@ -0,0 +1,12 @@
+public class SimpleInterest {
+    public static void main(String[]args){
+        double p=6000;
+        double t=6;
+        double r=2.5;
+        double SI=(p*t*r)/100;
+        System.out.println("PRINCIPAL AMOUNT: "+p);
+        System.out.println("TIME PERIOD : "+t+ "years");
+        System.out.println("RATE OF INTEREST: "+r+ "%");
+        System.out.println("THE SIMPLE INTEREST IS: "+SI);
+    }
+}
